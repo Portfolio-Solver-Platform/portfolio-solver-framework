0a094633772abb956462bc45f01995b9e6017fac
sofusl90 <sofus@laubel.net>
2025-12-03 11:58:06 +0100

    sync

--- src/minizinc_runner.rs
+++ src/minizinc_runner.rs
@@ -1,5 +1,6 @@
 use crate::input::{Args, OutputMode};
 use crate::solver_output;
+use std::borrow::Borrow;
 use std::fmt::format;
 use std::io;
 use std::process::Command;
@@ -43,7 +44,7 @@ pub fn run(solver: &str, args: &Args) -> io::Result<()> {
         let stdout = String::from_utf8_lossy(&output.stdout);
         println!("Solver Output:\n{}", stdout);
 
-        let parsed_output = solver_output::Output::parse(stdout);
+        let parsed_output = solver_output::Output::parse(stdout.borrow());
 
         // Optional: Parse the output here.
         // Tip: Use the JSON output flag (--json-stream) for easier parsing in Rust!
