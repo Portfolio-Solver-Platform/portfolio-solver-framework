b5c99a62c1617666673a806c06059036ff253126
Sofus Laubel <sofus@laubel.net>
2026-01-07 13:16:26 +0100

    add failure test

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -16,12 +16,16 @@ fn test_cli_success() {
     .stdout(contains("=========="));
 }
 
-// #[test]
-// fn test_cli_failure() {
-//     let mut cmd = cargo::cargo_bin!("my_project_name").unwrap();
-
-//     // Run without arguments
-//     cmd.assert()
-//         .failure() // Check that exit code is NOT 0
-//         .stderr(contains("Error: Missing name argument"));
-// }
+#[test]
+fn test_cli_failure() {
+    let path = assert_cmd::cargo::cargo_bin!("portfolio-solver-framework");
+    let mut cmd = Command::new(path);
+    cmd.args([
+        "tests/data/accap_instance6.dzn",
+        "--debug-verbosity",
+        "warning",
+    ])
+    .assert()
+    .failure()
+    .stderr(contains("ERROR"));
+}
