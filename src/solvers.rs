f614742a511de27ce04049789b8efaa7a2ef089b
Jonas Bork <59069012+jonas-bork@users.noreply.github.com>
2026-01-13 15:59:39 +0100

    Add Gecode to Dockerfile (#36)
    * Move old Dockerfile into its own folder
    
    * Add Gecode to the Dockerfile
    
    * Use MiniZinc source version 2.9.4 instead of 2.8.5
    
    * Fixed various issues

--- src/solvers.rs
+++ src/solvers.rs
@@ -2,7 +2,6 @@ pub const HUUB_ID: &str = "solutions.huub";
 pub const CP_SAT_ID: &str = "cp-sat";
 pub const COIN_BC_ID: &str = "org.minizinc.mip.coin-bc";
 pub const CHOCO_ID: &str = "org.choco.choco";
-#[allow(dead_code)]
 pub const GECODE_ID: &str = "org.gecode.gecode";
 #[allow(dead_code)]
 pub const GUROBI_ID: &str = "org.minizinc.mip.gurobi";
@@ -13,6 +12,6 @@ pub const PUMPKIN_ID: &str = "nl.tudelft.algorithmics.pumpkin";
 pub const SCIP_ID: &str = "org.minizinc.mip.scip";
 pub const YUCK_ID: &str = "yuck";
 
-pub const ALL_IDS: [&str; 8] = [
-    COIN_BC_ID, PICAT_ID, HUUB_ID, CP_SAT_ID, CHOCO_ID, PUMPKIN_ID, YUCK_ID, HIGHS_ID,
+pub const ALL_IDS: [&str; 9] = [
+    COIN_BC_ID, PICAT_ID, HUUB_ID, CP_SAT_ID, CHOCO_ID, GECODE_ID, PUMPKIN_ID, YUCK_ID, HIGHS_ID,
 ];
