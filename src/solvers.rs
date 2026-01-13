11f8581254ffa3844d388f14ce7864d070a19b64
Jonas Bork <jonas10bork@gmail.com>
2026-01-13 11:34:12 +0100

    Fix warnings and make HIGHS run by default in dynamic schedule

--- src/solvers.rs
+++ src/solvers.rs
@@ -2,14 +2,17 @@ pub const HUUB_ID: &str = "solutions.huub";
 pub const CP_SAT_ID: &str = "cp-sat";
 pub const COIN_BC_ID: &str = "org.minizinc.mip.coin-bc";
 pub const CHOCO_ID: &str = "org.choco.choco";
+#[allow(dead_code)]
 pub const GECODE_ID: &str = "org.gecode.gecode";
+#[allow(dead_code)]
 pub const GUROBI_ID: &str = "org.minizinc.mip.gurobi";
 pub const HIGHS_ID: &str = "org.minizinc.mip.highs";
 pub const PICAT_ID: &str = "org.picat-lang.picat";
 pub const PUMPKIN_ID: &str = "nl.tudelft.algorithmics.pumpkin";
+#[allow(dead_code)]
 pub const SCIP_ID: &str = "org.minizinc.mip.scip";
 pub const YUCK_ID: &str = "yuck";
 
-pub const ALL_IDS: [&str; 7] = [
-    HUUB_ID, CP_SAT_ID, COIN_BC_ID, CHOCO_ID, PICAT_ID, PUMPKIN_ID, YUCK_ID,
+pub const ALL_IDS: [&str; 8] = [
+    HUUB_ID, CP_SAT_ID, COIN_BC_ID, CHOCO_ID, PICAT_ID, PUMPKIN_ID, YUCK_ID, HIGHS_ID,
 ];
